bf4b18439e3a238ab03251b7388485495b1ff3f5
MoussaBoaz <deploy@moussaboaz.dev>
2026-02-13 15:53:37 +0000

    Fix: CI/CD - Add missing Controller.php, fix User factory, remove npm cache

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Database\Factories\UserFactory;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
